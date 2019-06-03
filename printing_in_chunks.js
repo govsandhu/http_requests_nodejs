54c639d76e817e4bac2ac748ae13c12f9036a74d
Govind Sandhu <gc.sandhu@gmail.com>
2019-06-03 18:18:05 +0000

    Step 1 - Printed in chunks

--- printing_in_chunks.js
+++ printing_in_chunks.js
@@ -1,3 +1,5 @@
+var http = require('http');
+
 function getAndPrintHTMLChunks () {
 
   var requestOptions = {
@@ -5,6 +7,20 @@ function getAndPrintHTMLChunks () {
     path: '/http-examples/step1.html'
   };
 
-  /* Add your code here */
+  http.get(requestOptions, function(response) {
+    response.on('data', function (chunk) {
+      console.log(chunk + "\n")
+    })
+  })
 
 }
+
+getAndPrintHTMLChunks();
+
+
+
+
+//Your first function getAndPrintHTMLChunks will utilize the https library to GET a given URL.
+// You can use the example above as a reference or template.
+// This function should console.log each chunk of data as it is received, concatenated with a newline character ('\n') so you can visualize each chunk.
+// Remember in this and each subsequent Step - you will always need to invoke (call) the function you wrote.
